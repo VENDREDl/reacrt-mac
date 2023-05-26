0672d4c8a13da35eeb72db9a85e4060f7cf1ebb2
vendredl <node.js_@kakao.com>
2023-05-26 16:33:43 +0900

    23.05.25 // Added Readme.js

---
+++ src/components/Readme.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+function Readme() {  
+  return (
+    <div>\
+        <h2>23.05.25</h2>
+        <ul>
+            <h3>개발 4시간째라 문제가 많습니다.. </h3>
+            <li>Header 추가 </li>
+            <li>Icon 추가 </li>
+            <li>Modal Resize / Drag 이벤트 추가 - 수정 필요 </li>
+            <li>음악 플레이어 기능 테스트 중 </li>
+            <li>예정) 노래 중복 재생 안되게</li>
+        </ul>
+        <h2>23.05.26</h2>
+        <ul>
+            <h3>음악 플레이어</h3>
+            <li>커버 이미지 추가</li>
+            <li>온/오프 기능 추가</li>
+            <li>|- 상단 메뉴 노래 부분(빨간부분) 클릭 시 음악 플레이어 온/오프 가능</li>
+            <li>오프 상태에서도 노래가 재생되도록 변경</li>
+        </ul>
+
+        
+    </div>
+  )
+}
+
+export default Readme;
